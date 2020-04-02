74e1e5a6e214cf564ee219051f61ff792c6d9017
Austin Brodeur <austinbrodeur@shaw.ca>
2020-04-01 18:35:41 -0600

    server tweak

--- atk2/simple_socket_server.c
+++ atk2/simple_socket_server.c
@@ -63,6 +63,9 @@ int main(void)
 
     // loop through all the results and bind to the first we can
     for(p = servinfo; p != NULL; p = p->ai_next) {
+
+        sleep(10); // added wait for attack 2 denial of service
+
         if ((sockfd = socket(p->ai_family, p->ai_socktype,
                 p->ai_protocol)) == -1) {
             perror("server: socket");
@@ -107,9 +110,6 @@ int main(void)
     printf("server: waiting for connections...\n");
 
     while(1) {  // main accept() loop
-
-        sleep(10); // added wait for attack 2 denial of service
-
         sin_size = sizeof their_addr;
         new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
         if (new_fd == -1) {
